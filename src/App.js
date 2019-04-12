b66cda372e3787cf46c0a96aaa622c12ac54f0fa
cedricgdt <cedric.gardianot@laposte.net>
2019-04-12 11:06:39 +0200

    first draft navbar, modal to need to be fixed, geoloc ok, API call ok

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import '../node_modules/materialize-css/dist/css/materialize.min.css'
 import './App.css';
 import NavBar from './Components/NavBar/NavBar.js'
 import Geolocation from './Components/Geolocation/Geolocation';
-//import M from '../node_modules/materialize-css/dist/js/materialize.js';
+//import EventModal from './Components/EventModal/EventModal'
+import M from '../node_modules/materialize-css/dist/js/materialize.js';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
       <div className="App">
         <NavBar />
         <Geolocation />
+        {/* <EventModal /> */}
       </div>
     );
   };
