6f2f68963a064000f8dc701f2e897242e91353dd
cedricgdt <cedric.gardianot@laposte.net>
2019-04-12 10:07:48 +0200

    Creation of the event display structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import '../node_modules/materialize-css/dist/css/materialize.min.css'
+import M from '../node_modules/materialize-css/dist/js/materialize.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+        <div className="App">
+          
+        </div>
     );
   }
 }
