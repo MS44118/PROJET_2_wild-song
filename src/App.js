e38336c8acb97867c4b8ca15888a99c53b45ad82
Besnard-Samantha <samantha.besnard.1993@gmail.com>
2019-04-12 10:06:57 +0200

    add good name for props to call the result api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import EventModal from "./Components/EventModal/EventModal";
 import '../node_modules/materialize-css/dist/css/materialize.min.css'
-import M from '../node_modules/materialize-css/dist/js/materialize.js';
+//import M from '../node_modules/materialize-css/dist/js/materialize.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <EventModal />
+
       </div>
-    );
+  );
   }
 }
 
--- src/Components/EventModal/EventModal.js
+++ src/Components/EventModal/EventModal.js
@@ -1,28 +1,26 @@
 import React, { Component } from "react";
 import { Modal, Button } from "react-materialize";
 import Event from "./Component/Event/Event"
-
+// This component call the component Event for display as many times as there is a geolocate event
 class EventModal extends Component {
   constructor(props){
     super();
   };
-
-const resultEventsApi = this.props.NOMDUPROPS.resultsPage.results.event;
-
   render(){
+    let resultEventsApi = this.props.songkick;
     return (
       <Modal header="Evenement à proximité " trigger={<Button>Géolocalisez-moi</Button>}>
         {resultEventsApi.map((event) => {
-              <Event title={resultEventsApi.performance[0].displayName } 
+        <Event  title={resultEventsApi.performance[0].displayName } 
                 image={resultEventsApi.performance[0].artist.id}
                 address1={resultEventsApi.venue.displayName} 
                 address2={resultEventsApi.location.city}
                 date={resultEventsApi.start.date}
                 time={resultEventsApi.start.time}
-              />
+        />
         })};
       </Modal>
-    )
+    );
   };
-}
+};
 export default EventModal;
