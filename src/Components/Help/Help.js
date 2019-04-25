188ad0d1b0c03ac1907c6d87dc86439f908880db
Tanguy Jouvin <tanguyj28@yahoo.fr>
2019-04-25 10:25:34 +0200

    modification on search and help modal

--- src/Components/Help/Help.js
+++ src/Components/Help/Help.js
@@ -37,7 +37,16 @@ class Help extends Component {
         </Button>
         <Modal id="modal1" header="Bienvenue dans l'aide">
           <p>Vous trouverez ici les informations d'aide pour utiliser de manière optimale l'application.</p>
-          <p>contributrices/eurs: Simple Pol, Cedric aka Git Master, Samantha, Bot ist and PePe the Toggle</p>
+          <p>contributrices/eurs</p>
+          <div class="chip">
+            <img src="./Images/BaptisteL.jpeg" alt="Contact Person"></img>
+            Jane Doe
+          </div>
+          <img class="materialboxed" width="1 em" src="./Images/BaptisteL.jpeg" alt="BaptisteL"></img>
+          <img class="materialboxed" width="1 em" src="./Images/MaelennS.jpeg" alt="MaelennS"></img>
+          <img class="materialboxed" width="1 em" src="./Images/SamanthaB.jpeg" alt="SamanthaB"></img>
+          <img class="materialboxed" width="1 em" src="./Images/CedricG.jpeg" alt="CedricG"></img>
+          <img class="materialboxed" width="1 em" src="./Images/TanguyJ.jpeg" alt="TanguyJ"></img>
           <version>V.0.02</version>
           <p>Dark Mode</p>
           <Switch offLabel="off" onLabel="On" onChange={this.state.pressed}/>
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,29 +1,52 @@
-import React from "react";
-import { Modal, Button } from "react-materialize";
+import React, { Component } from 'react';
+ import { Modal, Button } from "react-materialize";
+ import config from '../../config';
+// import EventModal from '../EventModal/EventModal';
 
-const Search = () => {
-  return (
-    <div>
-      <Button
-        icon="search"
-        flat waves="light" //to hide the button raising
-        node="a" //to activate the href
-        href="#modal-search" // link to the ID
-        floating //round button + icon color
-        large //icon size
-        className="modal-trigger transparent" // to trigger the Modal
-      >
-      </Button>
-      <Modal id="modal-search" header="Moteur de Recherche">
-        <p>Prochainement, vous trouverez ici la fonctionnalité RECHERCHE.</p>
-        <p>par ville</p>
-        <p>par artiste</p>
-        <p>par date</p>
-        <p>par prix</p>
-        <p>par type (exemple: filtre ENFANTS) </p>
-      </Modal>
-    </div>
-  ); 
-}
+class Search extends Component  {
+  constructor(){
+    super();
+
+    }
   
+
+//méthode de récupération fetch pour les concerts de l'API songkick, les concerts sont triés par nom d'artise, date, ville
+  getSearchConcert = () => {
+    fetch(`https://api.songkick.com/api/3.0/events.json?apikey=${config}&artist_name=patrick+bruel&location=geo:47.2172500,-1.5533600`)
+    .then(result =>result.json())
+    .then(result => {console.log(result.resultsPage.results.event[0].displayName)})    
+  }
+ 
+    render() {
+      
+      return (
+        <div>
+          
+          <Button
+            icon="search"
+            flat waves="light" //to hide the button raising
+            node="a" //to activate the href
+            href="#modal-search" // link to the ID
+            floating //round button + icon color
+            large //icon size
+            className="modal-trigger transparent" // to trigger the Modal
+          >
+          </Button>
+          <Modal id="modal-search" header="Moteur de Recherche">
+            <p>Prochainement, vous trouverez ici la fonctionnalité RECHERCHE.</p>
+            <p>par ville</p>
+            <p>par artiste</p>
+            <p>par date</p>
+            <p>par prix</p>
+            <p>par type (exemple: filtre ENFANTS) </p>
+          </Modal>
+        </div>
+      ); 
+    }
+
+  }
+
+  
+
+
 export default Search;
